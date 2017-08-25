197db84e97b39b12992e457ede926e65469df7dc
Yan Martins <yanlm1998@gmail.com>
2017-08-25 10:39:02 -0300

    Classe Complexo e classe Principal estão prontas

--- aula9/src/main/java/Complexo.java
+++ aula9/src/main/java/Complexo.java
@@ -1,2 +1,28 @@
 public class Complexo {
+
+    private int real;
+    private int imaginario;
+
+    public Complexo(){
+        real = 0;
+        imaginario = 0;
+    }
+
+    public Complexo(int r, int i){
+        real = r;
+        imaginario = i;
+    }
+
+    public void soma(Complexo a, Complexo b){
+        real = a.real + b.real;
+        imaginario = a.imaginario + b.imaginario;
+    }
+    public void subtracao(Complexo a, Complexo b){
+        real = a.real - b.real;
+        imaginario = a.imaginario - b.imaginario;
+    }
+
+    public String toString(){
+        return "("+real+","+imaginario+")";
+    }
 }
--- aula9/src/main/java/Principal.java
+++ aula9/src/main/java/Principal.java
@@ -1,2 +1,26 @@
 public class Principal {
+
+    public static void main(String[] args) {
+
+        Complexo x = new Complexo(2,3);
+        Complexo y = new Complexo(6,3);
+
+        Complexo j = new Complexo();
+        Complexo g = new Complexo();
+
+        j.soma(x,y);
+        System.out.println(j.toString());
+
+        j.soma(g, x);
+        System.out.println(j.toString());
+
+        x.subtracao(x,y);
+        System.out.println(x.toString());
+
+        j.subtracao(g,y);
+        System.out.println(j.toString());
+
+        j.soma(j,j);
+        System.out.println(j.toString());
+    }
 }
